91912b8994d4b6f2731dee4fae25e382bd2a4d7c
Joffre Veloz <joffreandres11@gmail.com>
2025-08-05 14:22:55 -0500

    Update page.tsx

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -2,15 +2,23 @@
 import { createProduct } from "@/actions";
 import ProductForm from "@/components/admin/product-form";
 import { Product } from "@/components/product/interface";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
 
 export default function CreateProductPage() {
+  const router = useRouter();
   const [editingProduct] = useState<Product | undefined>();
 
   const handleSave = async (product: Partial<Product>) => {
     try {
       const res = await createProduct(product);
-      console.log("Product created:", res);
+      if (res.statusCode === 201) {
+        toast.success(res.message);
+        router.push("/admin/products");
+      } else {
+        toast.error(res.message);
+      }
     } catch (error) {
       console.error("Error creating product:", error);
     }
